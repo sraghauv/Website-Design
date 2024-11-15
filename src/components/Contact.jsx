ad62214af08eaa5d75a9159be2f7b82fae3f5b8f
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-11-14 19:07:03 -0600

    Fixed display of the page to display the correct page!

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <div>
       <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-600">About Page</h1>
+        <h1 className="text-4xl font-bold text-blue-600">Contact Page</h1>
       </div>
       <SponsorsWidget />
     </div>
