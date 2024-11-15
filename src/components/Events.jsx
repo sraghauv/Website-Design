efd12f7d20522333f88281653ee558bf19b34f65
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-11-14 19:04:48 -0600

    Implemented Events component responsible for containing all the content for Events page to the website!

---
+++ src/components/Events.jsx
@@ -0,0 +1,14 @@
+import SponsorsWidget from "./SponsorWidget";
+
+const Events = () => {
+  return (
+    <div>
+      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">Events Page</h1>
+      </div>
+      <SponsorsWidget />
+    </div>
+  );
+};
+
+export default Events;
