a46fde8b553da968ca5288b80a0ff84da74102c9
Bryan <bryan.doan45@gmail.com>
2024-11-17 17:47:02 -0600

    background

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import AboutInfo from "./AboutInfo"
 import OfficerSection from "./OfficerSection"
 import SponsorsWidget from "../SponsorWidget";
+import "./About.css"
 
 const About = () => {
   return (
-    <div>
+    <div className="about">
       <AboutInfo />
       <OfficerSection />  
       <SponsorsWidget />
