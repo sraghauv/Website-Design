2fe758f405d7203131b27c1bf1db173bc0bb41d9
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-11-16 23:25:21 -0600

    Added comments for the new JavaScript file handling export of all components needed for Events page and the planned News and Post components!

--- src/components/Events/News.jsx
+++ src/components/Events/News.jsx
@@ -1,1 +1,6 @@
-// News component for the Events page
+//  Where the News component for the Events page is created and exported 
+
+/* 
+    Needed API Key for the API to get data from Instagram posts 
+    from LHNT's Instagram Account to here to get started!
+*/
--- src/components/Events/events.js
+++ src/components/Events/events.js
@@ -1,3 +1,11 @@
+/* 
+    This exports all the components for the Events
+    page from each of their JSX files inside the 
+    Events folder, so it's easier to import multiple 
+    components needed for the Events page
+    from this file!
+*/
+
 import Event from "./Event";
 import EventsDisplay from "./EventsDisplay";
 import Filter from "./Filter";
